1f41d9146fd64fbabce7bab90ab3136b7eca4ebf
Pavel <Papaskaaaa@gmail.com>
2021-05-11 00:13:02 +0300

    header

---
+++ Code/Index/Script.js
@@ -0,0 +1,36 @@
+//hamburger
+document.querySelector(".hamburger nav").style.height = "0px"
+
+function hamburger(){ 
+    let hamb = document.querySelector(".hamburger nav").style.display
+
+    if(hamb === ""){
+        let coord_open_hamb = 0
+        document.querySelector(".hamburger img").src = "../../Source/Img/open hamb.svg"
+        document.querySelector(".hamburger nav").style.display = "block"
+
+        let open_hamb = setInterval(function(){
+            if(coord_open_hamb < 201){
+                coord_open_hamb+=10
+                document.querySelector(".hamburger nav").style.height = coord_open_hamb + "px"
+            }
+            else{
+                setTimeout(() => clearInterval(open_hamb))
+            }
+        },1)
+        
+    }else{
+        let coord_closed_hamb = 200
+        document.querySelector(".hamburger img").src = "../../Source/Img/closed hamb.svg"
+
+        let closed_hamb = setInterval(function(){
+            if(coord_closed_hamb > -1){
+                coord_closed_hamb-=10
+                document.querySelector(".hamburger nav").style.height = coord_closed_hamb + "px"
+            }else{
+                document.querySelector(".hamburger nav").style.display = ""
+                setTimeout(() => clearInterval(closed_hamb))
+            }
+        },1)
+    }
+} 
